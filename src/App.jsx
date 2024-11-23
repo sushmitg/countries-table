1bc9176aa3973e22d1d8254f211c51c008851e34
Sushmit <er.sushmit@gmail.com>
2024-11-23 08:20:07 +0530

    Code Splitting and performance improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Filter from './components/Filter/Filter.jsx';
-import ShowTableButton from './components/ShowTableButton/ShowTableButton.jsx';
-import CountriesTable from './components/CountriesTable/CountriesTable.jsx';
+import React, { useState, Suspense, lazy, useCallback } from "react";
+import axios from "axios";
+import Filter from "./components/Filter/Filter.jsx";
+import ShowTableButton from "./components/ShowTableButton/ShowTableButton.jsx";
 
-import './App.css';
+const CountriesTable = lazy(() =>
+  import("./components/CountriesTable/CountriesTable.jsx")
+);
+
+import "./App.css";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -12,67 +15,76 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
 
-
   const fetchData = async () => {
-
     setIsLoading(true);
 
     try {
-      const response = await axios.get('https://api.sampleapis.com/countries/countries');
+      const response = await axios.get(
+        "https://api.sampleapis.com/countries/countries"
+      );
       setCountries(response.data);
       setFilteredCountries(response.data);
     } catch (error) {
-      console.error('Error fetching data: ', error);
+      console.error("Error fetching data: ", error);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-
   };
 
-  const applyFilters = (nameFilter, populationFilter) => {
-    let filtered = countries;
+  const applyFilters = useCallback(
+    (nameFilter, populationFilter) => {
+      let filtered = countries;
 
-    // Apply name filter
-    if (nameFilter) {
-      filtered = filtered.filter(country =>
-        country.name.toLowerCase().includes(nameFilter.toLowerCase())
-      );
-    }
-
-    // Apply population filter
-    if (populationFilter) {
-      switch (populationFilter) {
-        case '1M':
-          filtered = filtered.filter(country => country.population < 1000000);
-          break;
-        case '5M':
-          filtered = filtered.filter(country => country.population < 5000000);
-          break;
-        case '10M':
-          filtered = filtered.filter(country => country.population < 10000000);
-          break;
-        default:
-          break;
+      // Apply name filter
+      if (nameFilter) {
+        filtered = filtered.filter((country) =>
+          country.name.toLowerCase().includes(nameFilter.toLowerCase())
+        );
       }
-    }
 
-    setFilteredCountries(filtered);
-  };
+      // Apply population filter
+      if (populationFilter) {
+        switch (populationFilter) {
+          case "1M":
+            filtered = filtered.filter(
+              (country) => country.population < 1000000
+            );
+            break;
+          case "5M":
+            filtered = filtered.filter(
+              (country) => country.population < 5000000
+            );
+            break;
+          case "10M":
+            filtered = filtered.filter(
+              (country) => country.population < 10000000
+            );
+            break;
+          default:
+            break;
+        }
+      }
 
-  const toggleShowTable = () => {
+      setFilteredCountries(filtered);
+    },
+    [countries]
+  );
+
+  const toggleShowTable = useCallback(() => {
+    console.log("toggleShowTable");
+
     if (countries.length === 0) {
       fetchData();
     }
-    
+
     setShowTable(!showTable);
-  };
+  });
 
   return (
-    <div className='country-container'>
+    <div className="country-container">
       <div className="country-header">
-
         <h1 className="country-title">Countries Info</h1>
-        
+
         <div className="country-filters-container">
           <Filter
             showTable={showTable}
@@ -80,13 +92,19 @@ const App = () => {
             setFilteredCountries={setFilteredCountries}
             applyFilters={applyFilters}
           />
-          <ShowTableButton showTable={showTable} toggleShowTable={toggleShowTable} />
+          <ShowTableButton
+            showTable={showTable}
+            toggleShowTable={toggleShowTable}
+          />
         </div>
       </div>
       <div className="country-body">
-        {showTable && <CountriesTable isLoading={isLoading} data={filteredCountries} />}
+        {showTable && (
+          <Suspense fallback={<p>Loading...</p>}>
+            <CountriesTable isLoading={isLoading} data={filteredCountries} />
+          </Suspense>
+        )}
       </div>
-      
     </div>
   );
 };
