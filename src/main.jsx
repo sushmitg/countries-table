9df96faf6051159205362e6ff40c11164db1be64
Sushmit <er.sushmit@gmail.com>
2024-11-30 23:13:53 +0530

    added ErrorBoundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   </StrictMode>
 );
