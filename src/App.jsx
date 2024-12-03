133bb07c673e25962f4728678ffd2c3c3147b57c
Sushmit <er.sushmit@gmail.com>
2024-12-03 11:28:16 +0530

    bug fix: api fetch call race condition handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Suspense, lazy, useCallback } from "react";
+import React, { useState, Suspense, lazy, useCallback, useRef } from "react";
 import { useSelector } from "react-redux";
 import { COUNTRIES_API_URL } from "../config.js";
 import useOnlineStatus from "./hooks/useOnline.js";
@@ -14,14 +14,22 @@ const CountriesTable = lazy(() =>
 import "./App.css";
 
 const App = () => {
+  // State hooks
   const [showTable, setShowTable] = useState(false);
-  const isOnline = useOnlineStatus(); // Track network status
 
+  // Custom hook to check online status
+  const isOnline = useOnlineStatus();
+
+  // Redux state and actions
   const { setCountries, setIsLoading, setIsFetchError } = useCountriesActions();
   const { isLoading, isFetchError, countries, filteredCountries } = useSelector(
     (state) => state.countriesReducer
   );
 
+  // Ref for AbortController
+  const abortControllerRef = useRef(null);
+
+  // Fetch countries data
   const fetchData = useCallback(async () => {
     if (!isOnline) {
       setIsFetchError(true); // Show error if offline
@@ -31,8 +39,14 @@ const App = () => {
     setIsLoading(true);
     setIsFetchError(false);
 
+    // Abort previous request if any
+    abortControllerRef.current?.abort();
+    abortControllerRef.current = new AbortController();
+
     try {
-      const response = await axios.get(COUNTRIES_API_URL);
+      const response = await axios.get(COUNTRIES_API_URL, {
+        signal: abortControllerRef.current?.signal,
+      });
 
       if (response.data.error) {
         setIsFetchError(true);
@@ -41,14 +55,19 @@ const App = () => {
       }
 
       setCountries(response.data);
-    } catch (error) {
-      setIsFetchError(true);
-      console.error("Error fetching data: ", error);
-    } finally {
       setIsLoading(false);
+    } catch (error) {
+      if (axios.isCancel(error)) {
+        console.log("Previous Request canceled", error.message);
+      } else {
+        // handle error
+        setIsFetchError(true);
+        console.error("Error fetching data: ", error);
+      }
     }
   }, [setIsLoading, setIsFetchError, setCountries]);
 
+  // Toggle table visibility and fetch data if needed
   const toggleShowTable = useCallback(() => {
     if (!showTable && countries.length === 0) {
       fetchData();
